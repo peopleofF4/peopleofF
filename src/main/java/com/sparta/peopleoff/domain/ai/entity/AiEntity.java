1b7f20314573f522284dd44f03faad4f1eb29c3e
choisojin <dvega.lyla@gmail.com>
2024-11-09 05:28:47 +0900

    [fix] 엔티티 테이블명 수정
    AiEntity : ai -> p_ai
    MenuEntity : menu -> p_menu
    ReviewEntity : reviews -> p_review

--- src/main/java/com/sparta/peopleoff/domain/ai/entity/AiEntity.java
+++ src/main/java/com/sparta/peopleoff/domain/ai/entity/AiEntity.java
@@ -1,16 +1,22 @@
 package com.sparta.peopleoff.domain.ai.entity;
 
 import com.sparta.peopleoff.domain.menu.entity.MenuEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.mapping.ToOne;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "ai")
+@Table(name = "p_ai")
 public class AiEntity {
 
   @Id

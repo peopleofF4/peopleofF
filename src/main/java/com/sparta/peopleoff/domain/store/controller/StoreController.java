4f12f715bf8b8000ffd6e12013673926d0f0836b
choisojin <dvega.lyla@gmail.com>
2024-11-09 06:06:59 +0900

    [feat] 가게조회 기능 구현
    전체 가게조회 / 단일 가게조회

--- src/main/java/com/sparta/peopleoff/domain/store/controller/StoreController.java
+++ src/main/java/com/sparta/peopleoff/domain/store/controller/StoreController.java
@@ -1,11 +1,16 @@
 package com.sparta.peopleoff.domain.store.controller;
 
+import com.sparta.peopleoff.domain.store.dto.StoreGetResponseDto;
 import com.sparta.peopleoff.domain.store.dto.StorePostRequestDto;
 import com.sparta.peopleoff.domain.store.entity.StoreEntity;
 import com.sparta.peopleoff.domain.store.service.StoreService;
 import com.sparta.peopleoff.domain.user.entity.UserEntity;
 import com.sparta.peopleoff.domain.user.service.UserService;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +44,17 @@ public class StoreController {
     StoreEntity store = storeService.registerStore(storeRequestDto, user);
 
     return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/{storeId}")
+  public ResponseEntity<StoreGetResponseDto> getStoreById(@PathVariable UUID storeId) {
+    StoreGetResponseDto store = storeService.getStoreById(storeId);
+    return ResponseEntity.ok(store);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<StoreGetResponseDto>> getAllStores() {
+    List<StoreGetResponseDto> stores = storeService.getAllStores();
+    return ResponseEntity.ok(stores);
   }
 }

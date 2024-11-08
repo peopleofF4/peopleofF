f772e4b7d77ad12646eb9585da4aa576c477c562
choisojin <dvega.lyla@gmail.com>
2024-11-09 06:20:45 +0900

    [feat] 가게수정 기능 구현 #6
    가게수정 기능 구현

--- src/main/java/com/sparta/peopleoff/domain/store/controller/StoreController.java
+++ src/main/java/com/sparta/peopleoff/domain/store/controller/StoreController.java
@@ -2,6 +2,7 @@ package com.sparta.peopleoff.domain.store.controller;
 
 import com.sparta.peopleoff.domain.store.dto.StoreGetResponseDto;
 import com.sparta.peopleoff.domain.store.dto.StorePostRequestDto;
+import com.sparta.peopleoff.domain.store.dto.StorePutRequestDto;
 import com.sparta.peopleoff.domain.store.entity.StoreEntity;
 import com.sparta.peopleoff.domain.store.service.StoreService;
 import com.sparta.peopleoff.domain.user.entity.UserEntity;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,5 +58,14 @@ public class StoreController {
   public ResponseEntity<List<StoreGetResponseDto>> getAllStores() {
     List<StoreGetResponseDto> stores = storeService.getAllStores();
     return ResponseEntity.ok(stores);
+  }
+
+  @PutMapping("/{storeId}")
+  public ResponseEntity<?> updateStore(
+      @PathVariable UUID storeId,
+      @RequestBody StorePutRequestDto storeUpdateRequestDto) {
+
+    storeService.updateStore(storeId, storeUpdateRequestDto);
+    return ResponseEntity.ok().build();
   }
 }

15f0743de1f40a44de0480a5456cc46f43f9cf54
choisojin <dvega.lyla@gmail.com>
2024-11-09 05:44:26 +0900

    [feat] 가게등록 기능 구현
    JWT 없어서 일단 유저 ID 클라이언트한테 받는 조건으로 구현

--- src/main/java/com/sparta/peopleoff/domain/store/service/StoreService.java
+++ src/main/java/com/sparta/peopleoff/domain/store/service/StoreService.java
@@ -1,5 +1,42 @@
 package com.sparta.peopleoff.domain.store.service;
 
+import com.sparta.peopleoff.domain.category.entity.CategoryEntity;
+import com.sparta.peopleoff.domain.category.repository.CategoryRepository;
+import com.sparta.peopleoff.domain.store.dto.StorePostRequestDto;
+import com.sparta.peopleoff.domain.store.entity.StoreEntity;
+import com.sparta.peopleoff.domain.store.repository.StoreRepository;
+import com.sparta.peopleoff.domain.user.entity.UserEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StoreService {
 
+  private final StoreRepository storeRepository;
+  private final CategoryRepository categoryRepository;
+
+  public StoreService(StoreRepository storeRepository, CategoryRepository categoryRepository) {
+    this.storeRepository = storeRepository;
+    this.categoryRepository = categoryRepository;
+  }
+
+  @Transactional
+  public StoreEntity registerStore(StorePostRequestDto storeRequestDto, UserEntity user) {
+
+    CategoryEntity category = categoryRepository.findByCategoryName(
+            storeRequestDto.getCategoryName())
+        .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 카테고리 이름입니다."));
+
+    StoreEntity newStore = new StoreEntity(
+        storeRequestDto.getStoreName(),
+        storeRequestDto.getStoreAddress(),
+        storeRequestDto.getStorePhoneNumber(),
+        storeRequestDto.getBusinessNumber(),
+        storeRequestDto.getMinimumOrderPrice(),
+        user,
+        category
+    );
+
+    return storeRepository.save(newStore);
+  }
 }

7b8a81a1605e1026be1fee75aa67e0a5baab9928
Woojin Kim <rladnwlsok@naver.com>
2024-11-11 00:12:29 +0900

    [Feat] 유저 검색 / ResBasicCode에 404 에러 추가 (@RequestParam으로 값을 가져오기 때문에 보안적으로 에러처리가 필요하다 생각) #4

--- src/main/java/com/sparta/peopleoff/domain/user/controller/ManagerController.java
+++ src/main/java/com/sparta/peopleoff/domain/user/controller/ManagerController.java
@@ -1,12 +1,15 @@
 package com.sparta.peopleoff.domain.user.controller;
 
 import com.sparta.peopleoff.common.apiresponse.ApiResponse;
+import com.sparta.peopleoff.domain.user.dto.UserResponseDto;
 import com.sparta.peopleoff.domain.user.service.ManagerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -16,9 +19,23 @@ public class ManagerController {
     private ManagerService managerService;
 
 //    @PutMapping("/user/{userId}")
-//    private ResponseEntity<ApiResponse<Long>> deleteUser(@PathVariable Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+//   private ResponseEntity<ApiResponse<Long>> deleteUser(@PathVariable Long userId,
+//                                                        @AuthenticationPrincipal UserDetailsImpl userDetails) {
 //        managerService.deleteUser(userId, userDetails.getUser());
 //        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.OK(userId));
 //    }
 
+    /**
+     * 유저 검색
+     * @param userName
+     * @return
+     */
+    @GetMapping("/store/search")
+    private ResponseEntity<ApiResponse<List<UserResponseDto>>> searchUser(
+            @RequestParam String userName
+//            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        List<UserResponseDto> responseDtos = managerService.searchUser(userName/*, userDetails*/);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.OK(responseDtos));
+    }
 }
--- src/main/java/com/sparta/peopleoff/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/peopleoff/domain/user/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.sparta.peopleoff.domain.user.repository;
 
+import com.sparta.peopleoff.domain.user.dto.UserResponseDto;
 import com.sparta.peopleoff.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    List<UserEntity> findByUsernameContaining(String userName);
 }

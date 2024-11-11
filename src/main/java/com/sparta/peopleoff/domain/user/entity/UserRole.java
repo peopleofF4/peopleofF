2c27e2077556d6f440195c8a8cb1e750c0462ec4
kimsung3113 <kimsung1017@naver.com>
2024-11-11 21:45:44 +0900

    [feat] User Controller, Service #9 회원가입, 회원 정보 조회 API 추가 responseDTO들 추가 UserRole Enum 추가

---
+++ src/main/java/com/sparta/peopleoff/domain/user/entity/UserRole.java
@@ -0,0 +1,31 @@
+package com.sparta.peopleoff.domain.user.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+
+  CUSTOMER(Authority.CUSTOMER),  // 사용자 권한
+  OWNER(Authority.OWNER),     // 가맹점 사장 권한
+  MANAGER(Authority.MANAGER), // 관리자 권한
+  MASTER(Authority.MASTER);  // 관리자 권한
+
+  private final String authority;
+
+  UserRole(String authority) {
+    this.authority = authority;
+  }
+
+  public String getAuthority() {
+    return this.authority;
+  }
+
+  public static class Authority {
+
+    public static final String CUSTOMER = "ROLE_CUSTOMER";
+    public static final String OWNER = "ROLE_OWNER";
+    public static final String MANAGER = "ROLE_MANAGER";
+    public static final String MASTER = "ROLE_MASTER";
+  }
+
+}

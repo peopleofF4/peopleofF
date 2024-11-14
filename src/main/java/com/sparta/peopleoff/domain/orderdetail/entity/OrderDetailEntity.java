fe792a5f626b04455599786b3b8c00d4ba763426
wooseok <wooseok6008@gmail.com>
2024-11-14 14:37:44 +0900

    [fix] OrderDetailEntity fetch 타입 변경 #13
    - FetchType.EAGER

--- src/main/java/com/sparta/peopleoff/domain/orderdetail/entity/OrderDetailEntity.java
+++ src/main/java/com/sparta/peopleoff/domain/orderdetail/entity/OrderDetailEntity.java
@@ -31,7 +31,7 @@ public class OrderDetailEntity {
   @Column(nullable = false)
   private int menuCount;
 
-  @ManyToOne(fetch = FetchType.LAZY)
+  @ManyToOne(fetch = FetchType.EAGER)
   @JoinColumn(name = "order_id", nullable = false)
   private OrderEntity order;
 

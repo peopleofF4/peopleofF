d68161b3d71b09efaaf313c1d01f8c54f1f5462d
kimsung3113 <kimsung1017@naver.com>
2024-11-14 15:34:59 +0900

    [fix] 탈퇴한 회원 로그인 못하게 변경 #9

--- src/main/java/com/sparta/peopleoff/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/peopleoff/domain/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.peopleoff.domain.user.repository;
 
+import com.sparta.peopleoff.common.enums.DeletionStatus;
 import com.sparta.peopleoff.domain.user.entity.UserEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
   Optional<UserEntity> findByEmail(String email);
+
+  Optional<UserEntity> findByEmailAndDeletionStatus(String email, DeletionStatus deletionStatus);
 
   Optional<UserEntity> findByPhoneNumber(String phoneNumber);
 

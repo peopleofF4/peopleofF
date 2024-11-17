96bed44f963f9b748cc237aec7731c7cad529b20
kimsung3113 <kimsung1017@naver.com>
2024-11-17 13:19:37 +0900

    [refactor] Category 도메인 service 추가 후 impl 클래스로 수정#4

--- src/main/java/com/sparta/peopleoff/domain/category/service/CategoryService.java
+++ src/main/java/com/sparta/peopleoff/domain/category/service/CategoryService.java
@@ -1,44 +1,12 @@
 package com.sparta.peopleoff.domain.category.service;
 
-import com.sparta.peopleoff.common.rescode.ResBasicCode;
 import com.sparta.peopleoff.domain.category.dto.CategoryRequestDto;
-import com.sparta.peopleoff.domain.category.entity.CategoryEntity;
-import com.sparta.peopleoff.domain.category.repository.CategoryRepository;
-import com.sparta.peopleoff.exception.CustomApiException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.UUID;
 
-@Service
-@RequiredArgsConstructor
-public class CategoryService {
+public interface CategoryService {
 
-    private final CategoryRepository categoryRepository;
+  void createCategory(CategoryRequestDto categoryRequestDto);
 
-    /**
-     * 카테고리 생성
-     * @param categoryRequestDto
-     */
-    @Transactional
-    public void createCategory(CategoryRequestDto categoryRequestDto) {
-        CategoryEntity category = new CategoryEntity(categoryRequestDto);
-        categoryRepository.save(category);
-    }
+  void updateCategory(UUID categoryId, CategoryRequestDto categoryRequestDto);
 
-    /**
-     * 카테고리 수정
-     * @param categoryId
-     * @param categoryRequestDto
-     */
-    @Transactional
-    public void updateCategory(UUID categoryId, CategoryRequestDto categoryRequestDto) {
-
-        // [예외2] - 존재하지 않는 categoryId
-        CategoryEntity category = categoryRepository.findById(categoryId).orElseThrow(() ->
-                new CustomApiException(ResBasicCode.BAD_REQUEST, "존재하지 않는 카테고리입니다."));
-
-        category.setCategoryName(categoryRequestDto.getCategoryName());
-    }
 }
---
+++ src/main/java/com/sparta/peopleoff/domain/category/service/CategoryServiceImpl.java
@@ -0,0 +1,47 @@
+package com.sparta.peopleoff.domain.category.service;
+
+import com.sparta.peopleoff.common.rescode.ResBasicCode;
+import com.sparta.peopleoff.domain.category.dto.CategoryRequestDto;
+import com.sparta.peopleoff.domain.category.entity.CategoryEntity;
+import com.sparta.peopleoff.domain.category.repository.CategoryRepository;
+import com.sparta.peopleoff.exception.CustomApiException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+  private final CategoryRepository categoryRepository;
+
+  /**
+   * 카테고리 생성
+   *
+   * @param categoryRequestDto
+   */
+  @Override
+  @Transactional
+  public void createCategory(CategoryRequestDto categoryRequestDto) {
+    CategoryEntity category = new CategoryEntity(categoryRequestDto);
+    categoryRepository.save(category);
+  }
+
+  /**
+   * 카테고리 수정
+   *
+   * @param categoryId
+   * @param categoryRequestDto
+   */
+  @Override
+  @Transactional
+  public void updateCategory(UUID categoryId, CategoryRequestDto categoryRequestDto) {
+
+    // [예외2] - 존재하지 않는 categoryId
+    CategoryEntity category = categoryRepository.findById(categoryId).orElseThrow(() ->
+        new CustomApiException(ResBasicCode.BAD_REQUEST, "존재하지 않는 카테고리입니다."));
+
+    category.setCategoryName(categoryRequestDto.getCategoryName());
+  }
+}
